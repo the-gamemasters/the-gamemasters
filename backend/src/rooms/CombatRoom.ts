f3bd314d6c54300b17a1df3b5ef005304c57a70b
Carson Saunders <carsonsaundersalt@gmail.com>
2021-09-27 17:13:25 -0600

    started Battle view, continued combat logic on backend

--- backend/src/rooms/CombatRoom.ts
+++ backend/src/rooms/CombatRoom.ts
@@ -1,6 +1,30 @@
 import { Room, Client } from 'colyseus';
 import { CombatRoomState } from './schema/CombatRoomState';
 
+interface mockBattle {
+    party1: {
+        id: string,
+        displayName: string,
+        hp: number,
+        stats: {
+            strength: number;
+            dexterity: number;
+            constitution: number;
+            intelligence: number;
+        }
+    },
+    party2: {
+        id: string,
+        displayName: string,
+        hp: number,
+        stats: {
+            strength: number;
+            dexterity: number;
+            constitution: number;
+            intelligence: number;
+        }
+    }
+}
 export class CombatRoom extends Room<CombatRoomState> {
     maxClients = 2;
     onCreate(options: any) {
@@ -8,31 +32,102 @@ export class CombatRoom extends Room<CombatRoomState> {
 
         this.onMessage('turn', (client, message) => {
             console.log(`${client.sessionId} used ${message.action}`);
+
             switch (message.action) {
                 case 'attack':
                     if (this.state.currentTurn === 'party1') {
-                        this.state.party2.hp -= this.state.party1.stats.strength;
-                        if (this.state.party2.hp <= 0) {
-                            //TODO add death logic for party 2 death
+                        let damageDealt = Math.floor(((this.state.party1.stats.strength + this.state.party1.weaponBonus) * (Math.random() * (1.2 - .8) + .8)))
+                        console.log('P1 Attack:', damageDealt)
+                        this.state.party2.currentHp -= damageDealt;
+
+                        if (this.state.party2.currentHp <= 0) {
+                            this.state.party2.currentHp = 0;
+                            this.broadcast('victory', 'party1')
                         }
+                        this.state.currentTurn = 'party2'
                     } else {
-                        this.state.party1.hp -= this.state.party2.stats.strength;
-                        if (this.state.party1.hp <= 0) {
-                            //TODO add death logic for party 1 death
+                        let damageDealt = Math.floor(((this.state.party2.stats.strength + this.state.party2.weaponBonus) * (Math.random() * (1.2 - .8) + .8)))
+                        console.log('P2 Attack:', damageDealt)
+                        this.state.party1.currentHp -= damageDealt;
+
+                        if (this.state.party1.currentHp <= 0) {
+                            this.state.party1.currentHp = 0;
+                            this.broadcast('victory', 'party2')
                         }
+                        this.state.currentTurn = 'party1'
                     }
+                    break;
+
+                case 'spell':
+                    if (this.state.currentTurn === 'party1') {
+                        this.state.currentTurn = 'party2'
+                    } else {
+                        this.state.currentTurn = 'party1'
+                    }
+                    break;
+
+                case 'item':
+                    if (this.state.currentTurn === 'party1') {
+
+                    } else {
+
+                    }
+                    break;
+
+                case 'evade':
+                    if (this.state.currentTurn === 'party1') {
+
+                    } else {
+
+                    }
+                    break;
             }
+
         });
+
+        this.onMessage('debug', (client, message) => {
+            if (message === 'party1') {
+                console.log(JSON.stringify(this.state.party1))
+            } else if (message === 'party2') {
+                console.log(JSON.stringify(this.state.party2))
+            }
+
+        })
     }
 
     onJoin(client: Client, options: any) {
-        console.log(client.sessionId, 'joined!');
-        if (this.clients.length === 0) {
+        if (this.clients.length === 1) {
+            console.log(client.sessionId, 'joined as P1!');
             this.state.party1.id = client.sessionId;
-            client.send('assignment', 1)
-        } else {
+            client.send('assignment', 1);
+
+            //Mock Party1 "Charles"
+            this.state.party1.displayName = "Charles";
+            this.state.party1.weaponBonus = 1;
+            this.state.party1.stats.strength = 15;
+            this.state.party1.stats.dexterity = 6;
+            this.state.party1.stats.constitution = 11;
+            this.state.party1.maxHp = this.state.party1.stats.constitution * 10;
+            this.state.party1.currentHp = this.state.party1.maxHp;
+            this.state.party1.stats.intelligence = 8;
+
+        } else if (this.clients.length === 2) {
+            console.log(client.sessionId, 'joined as P2!');
             this.state.party2.id = client.sessionId;
+            client.send('assignment', 2);
+            //Mock Party2 "Giant Rat"
+            this.state.party2.displayName = "Giant Rat";
+            this.state.party2.weaponBonus = 4;
+            this.state.party2.stats.strength = 5;
+            this.state.party2.stats.dexterity = 9;
+            this.state.party2.stats.constitution = 13;
+            this.state.party2.maxHp = this.state.party2.stats.constitution * 10;
+            this.state.party2.currentHp = this.state.party2.maxHp;
+            this.state.party2.stats.intelligence = 0;
+            this.broadcast("ready")
         }
+
+
         //TODO Connect to backend, which connects to DB, to get player stats, items, and spells
     }
 
@@ -44,3 +139,5 @@ export class CombatRoom extends Room<CombatRoomState> {
         console.log('room', this.roomId, 'disposing...');
     }
 }
+
+
--- frontend/src/components/Combat/Combat.tsx
+++ frontend/src/components/Combat/Combat.tsx
@@ -1,27 +1,56 @@
-import React, { Component } from 'react';
-import * as Colyseus from 'colyseus.js';
+import React, { Component } from "react";
+import * as Colyseus from "colyseus.js";
+import "./styles/combat.css";
+import MoveBox from "./MoveBox";
 
 interface Props {}
 interface State {
     roomInstance: Colyseus.Room;
+    stateInstance: any;
     lastMove: string | undefined;
     loading: boolean;
+    p1Ready: boolean;
+    p2Ready: boolean;
+    myParty: string;
+    currentTurn: string;
 }
 
 export default class Combat extends Component<Props, State> {
     componentWillMount() {
-        this.setState({ lastMove: '', loading: true });
+        this.setState({ lastMove: "", loading: true });
     }
     componentDidMount() {
-        let client = new Colyseus.Client('ws://localhost:3553');
-        client.joinOrCreate('combat').then((room) => {
-            this.setState({ roomInstance: room, loading: false });
-            room.onMessage('assignment', (message) => {
-                // if (message) {
-
-                // }
+        let client = new Colyseus.Client("ws://localhost:3553");
+        client.joinOrCreate("combat").then((room) => {
+            //* Function to handle handing out party1 or party2 status
+            room.onMessage("assignment", (message) => {
+                if (message === 1) {
+                    this.setState({ myParty: "party1", p1Ready: true });
+                } else {
+                    this.setState({ myParty: "party2", p2Ready: true });
+                }
                 console.log(message);
             });
+
+            room.onMessage("ready", () => {
+                this.setState({
+                    p1Ready: true,
+                    p2Ready: true,
+                });
+            });
+
+            room.onMessage("victory", (message) => {
+                console.log(message);
+            });
+
+            //* Function to grab state updates
+            room.onStateChange((state) => {
+                this.setState({ stateInstance: state });
+                this.setState({
+                    currentTurn: this.state.stateInstance.currentTurn,
+                });
+            });
+            this.setState({ roomInstance: room, loading: false });
         });
     }
 
@@ -31,11 +60,11 @@ export default class Combat extends Component<Props, State> {
             action: move,
             moveData: {
                 item: null,
-                spell: 'fireball',
+                spell: "fireball",
             },
         };
 
-        room.send('turn', message);
+        room.send("turn", message);
 
         this.setState({ lastMove: move });
     };
@@ -44,34 +73,106 @@ export default class Combat extends Component<Props, State> {
     //     if (roominstance)
     // }
 
+    handleDebug = (party: string) => {
+        let room = this.state.roomInstance;
+        room.send("debug", party);
+    };
+
     render() {
         if (this.state.loading === true) {
-            return <div>Loading...</div>;
+            return <div className="page-container">Loading...</div>;
         } else {
             return (
-                <div>
-                    <h1>Combat</h1>
-                    <div>
-                        <button onClick={() => this.handleAction('attack')}>
-                            Attack
-                        </button>
-                        <button onClick={() => this.handleAction('spell')}>
-                            Spells
-                        </button>
-                        <button onClick={() => this.handleAction('item')}>
-                            Items
-                        </button>
-                        <button onClick={() => this.handleAction('evade')}>
-                            Evade
-                        </button>
+                <div className="page-container">
+                    <div className="combat-top">
+                        <h1 className="nes-text">Combat</h1>
+                        <h6>Room ID: {this.state.roomInstance.id}</h6>
                     </div>
-                    <br />
-                    <span>{this.state.lastMove}</span>
-                    <br />
-                    {console.log(this.state.roomInstance)}
-                    <span>
-                        Enemy HP: {this.state.roomInstance.state.party2.hp}
-                    </span>
+                    <div className="combat-mid">
+                        <div className="party1-sprite-box">
+                            <img
+                                src="https://cdn2.scratch.mit.edu/get_image/gallery/25124327_170x100.png"
+                                alt="donkey-kong-6065405-640"
+                                className="party1-sprite"
+                            />
+                        </div>
+
+                        <div className="mid-center"></div>
+                        <div className="party2-sprite-box">
+                            <img
+                                src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/3c1fb523717421.56327b1b5db32.gif"
+                                alt="giant rat"
+                                className="party2-sprite"
+                            />
+                        </div>
+                    </div>
+                    <div className="combat-bottom">
+                        {this.state.p1Ready ? (
+                            <div className="party-info-box party1-info">
+                                <span className="party-name">
+                                    {
+                                        this.state.stateInstance.party1
+                                            .displayName
+                                    }
+                                </span>
+                                <progress
+                                    className="nes-progress is-error hp-bar"
+                                    value={
+                                        this.state.stateInstance.party1
+                                            .currentHp
+                                    }
+                                    max={this.state.stateInstance.party1.maxHp}
+                                />
+                                <span className="hp-text">
+                                    {this.state.stateInstance.party1.currentHp}{" "}
+                                    / {this.state.stateInstance.party1.maxHp}
+                                </span>
+                                <button
+                                    type="button"
+                                    className="nes-btn"
+                                    onClick={() => this.handleDebug("party1")}>
+                                    Debug P1
+                                </button>
+                            </div>
+                        ) : (
+                            <div></div>
+                        )}
+                        <MoveBox
+                            handleAction={this.handleAction}
+                            currentTurn={this.state.currentTurn}
+                            myParty={this.state.myParty}
+                            loading={this.state.loading}
+                        />
+
+                        {this.state.p2Ready ? (
+                            <div className="party-info-box party2-info">
+                                <span className="party-name">
+                                    {
+                                        this.state.stateInstance.party2
+                                            .displayName
+                                    }
+                                </span>
+                                <progress
+                                    className="nes-progress is-error hp-bar flipped-hp"
+                                    value={
+                                        this.state.stateInstance.party2
+                                            .currentHp
+                                    }
+                                    max={this.state.stateInstance.party2.maxHp}
+                                />
+                                <span className="hp-text">
+                                    {this.state.stateInstance.party2.currentHp}{" "}
+                                    / {this.state.stateInstance.party2.maxHp}
+                                </span>
+                                <button
+                                    type="button"
+                                    className="nes-btn"
+                                    onClick={() => this.handleDebug("party2")}>
+                                    Debug P2
+                                </button>
+                            </div>
+                        ) : null}
+                    </div>
                 </div>
             );
         }
