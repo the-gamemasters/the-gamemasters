496162780402f8f80efbfe037a6e87ae765e4f65
Nathaniel Shirts <nshirts7@gmail.com>
2021-09-28 20:16:28 -0600

    set up massive and session in the server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
+
 import http from 'http';
 import express, { application } from 'express';
 import cors from "cors";
@@ -7,10 +10,43 @@ import bcrypt from 'bcrypt';
 import session from 'express-session';
 import massive from 'massive';
 import {createCharacter} from './controllers/characters'
+const { MASSIVE_HOST, MASSIVE_PORT, MASSIVE_DATABASE, MASSIVE_USER, MASSIVE_PASSWORD, SECRET, PORT } = process.env;
+
+// this prevents `Error: self signed certificate`, Will need to look into this when going to production.
+// @ts-ignore
+process.env[`NODE_TLS_REJECT_UNAUTHORIZED`] = 0;
+
 
 
 const app = express();
 const port = Number(process.env.PORT || 3553);
+
+
+app.use(
+    session({
+      resave: false,
+      saveUninitialized: true,
+      secret: SECRET,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+      },
+    })
+  );
+
+  massive({
+    host: MASSIVE_HOST,
+    port: +MASSIVE_PORT,
+    database: MASSIVE_DATABASE,
+    user: MASSIVE_USER,
+    password: MASSIVE_PASSWORD,
+    ssl: true,
+  })
+    .then((dbInstance) => {
+      app.set('db', dbInstance);
+      console.log('Database running, cool game is coming!!!',);
+      console.log(dbInstance)
+    })
+    .catch((err) => console.log('Database error', err));
 
 app.use(cors());
 app.use(express.json());
