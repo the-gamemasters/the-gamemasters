ac8315d66a662929aea7983191d2b25ed0b3a168
mflanders06 <mflanders06@gmail.com>
2021-11-01 21:47:30 -0600

    final prep for merge to master

--- frontend/src/components/CharacterCreation/CharacterCreation.tsx
+++ frontend/src/components/CharacterCreation/CharacterCreation.tsx
@@ -8,190 +8,193 @@ import BackgroundMusic from "../General/BackgroundMusic"
 // need to fix this to use the right dispatch
 import { useAppSelector, useAppDispatch } from "../../redux/reduxHooks"
 import {
-    setUserId,
-    setCharId,
-    setCharInfo,
-    selectUserId,
-    selectCharId,
-    selectCharInfo,
+	setUserId,
+	setCharId,
+	setCharInfo,
+	selectUserId,
+	selectCharId,
+	selectCharInfo,
 } from "../../redux/userSlice"
 
 interface Props {}
 
 const Background = styled.div`
-    height: 100vh;
-    background-image: url("/images/login-background.gif");
-    background-size: cover;
-    background-position: center bottom;
-    display: flex;
-    justify-content: space-between;
+	height: 100vh;
+	background-image: url("/images/login-background.gif");
+	background-size: cover;
+	background-position: center bottom;
+	display: flex;
+	justify-content: space-between;
 `
 
 const Title = styled.h1`
-    padding-top: 2.5rem;
-    color: #ffffff;
-    -webkit-text-stroke: 1px black;
+	padding-top: 2.5rem;
+	color: #ffffff;
+	-webkit-text-stroke: 1px black;
 `
 
 const LeftBox = styled.div`
-    height: 100vh;
-    width: 40vw;
-    text-align: left;
-    margin-left: 3rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    padding-top: 4rem;
-    padding-bottom: 10rem;
+	height: 100vh;
+	width: 40vw;
+	text-align: left;
+	margin-left: 3rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	padding-top: 4rem;
+	padding-bottom: 10rem;
 `
 
 const RightBox = styled.div`
-    height: 90vh;
-    width: 40vw;
-    overflow-y: auto;
-    border-radius: 5px;
-    background-color: rgba(255, 255, 255, 0.5);
-    border: 3px black solid;
-    margin-top: 5vh;
-    margin-right: 1vw;
-    padding: 10px;
-    display: flex;
-    flex-direction: column;
-    text-align: left;
+	height: 90vh;
+	width: 40vw;
+	overflow-y: auto;
+	border-radius: 5px;
+	background-color: rgba(255, 255, 255, 0.5);
+	border: 3px black solid;
+	margin-top: 5vh;
+	margin-right: 1vw;
+	padding: 10px;
+	display: flex;
+	flex-direction: column;
+	text-align: left;
 `
 
 const Input = styled.div`
-    font-size: 3 rem;
-    color: #ffffff;
-    -webkit-text-stroke: 1px black;
+	font-size: 3 rem;
+	color: #ffffff;
+	-webkit-text-stroke: 1px black;
 `
 
 export default function CharacterCreation(props: Props): ReactElement {
-    const [charName, setCharName] = useState<string>("")
-    const [currentClass, setCurrentClass] = useState<
-        "KNIGHT" | "ROGUE" | "BARBARIAN" | "MAGE" | "NONE"
-    >("NONE")
-    const [description, setDescription] = useState<string>("")
-    const dispatch = useAppDispatch()
-    const userId = useAppSelector(selectUserId)
-    const charId = useAppSelector(selectCharId)
+	const [charName, setCharName] = useState<string>("")
+	const [currentClass, setCurrentClass] = useState<
+		"KNIGHT" | "ROGUE" | "BARBARIAN" | "MAGE" | "NONE"
+	>("NONE")
+	const [description, setDescription] = useState<string>("")
+	const dispatch = useAppDispatch()
+	const userId = useAppSelector(selectUserId)
+	const charId = useAppSelector(selectCharId)
 
-    if (!userId){return (<Redirect to="/" />)}
-    if (charId){return (<Redirect to="/home" />)}
-    return (
-        <Background>
-            <BackgroundMusic musicSrc={"audio/music/track11-login.mp3"} />
-            <LeftBox>
-                <div>
-                    <Title>Enter Character Name!!</Title>
-                    <input
-                        type="string"
-                        value={charName}
-                        name="character name"
-                        onChange={(e) => setCharName(e.target.value)}
-                        placeholder="Character name"
-                    />
-                </div>
-                <div>
-                    <Title>Enter Character Description</Title>
-                    <textarea
-                        name="Character description"
-                        placeholder="Please enter a description of your character"
-                        value={description}
-                        cols={50}
-                        rows={5}
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <Title>Pick a class </Title>
-                    <div>
-                        <Input>
-                            <input
-                                type="radio"
-                                value="KNIGHT"
-                                name="class"
-                                onChange={() => setCurrentClass("KNIGHT")}
-                            />
-                            Knight
-                        </Input>
-                        <Input>
-                            <input
-                                type="radio"
-                                value="ROGUE"
-                                name="class"
-                                onChange={() => setCurrentClass("ROGUE")}
-                            />
-                            Rogue
-                        </Input>
-                        <Input>
-                            <input
-                                type="radio"
-                                value="BARBARIAN"
-                                name="class"
-                                onChange={(e) => setCurrentClass("BARBARIAN")}
-                            />
-                            Barbarian
-                        </Input>
-                        <Input>
-                            <input
-                                type="radio"
-                                value="MAGE"
-                                name="class"
-                                onChange={(e) => setCurrentClass("MAGE")}
-                            />
-                            Mage
-                        </Input>
-                    </div>
-                </div>
-            </LeftBox>
-            {currentClass === "NONE" ? null : (
-                <RightBox>
-                    <CharacterInfo currentClass={currentClass} />
-                    <Link to="/home">
-                        <button
-                            onClick={async () => {
-                                const result = await axios.post(
-                                    "/api/character",
-                                    {
-                                        userKey: userId,
-                                        charName,
-                                        description,
-                                        strength: classList[currentClass].str,
-                                        constitution:
-                                            classList[currentClass].con,
-                                        intelligence:
-                                            classList[currentClass].int,
-                                        dexterity: classList[currentClass].dex,
-                                    }
-                                )
+	if (!userId) {
+		return <Redirect to="/" />
+	}
+	if (charId) {
+		return <Redirect to="/home" />
+	}
+	return (
+		<Background>
+			<BackgroundMusic musicSrc={"audio/music/track11-login.mp3"} />
+			<LeftBox>
+				<div>
+					<Title>Enter Character Name!!</Title>
+					<input
+						type="string"
+						value={charName}
+						name="character name"
+						onChange={(e) => setCharName(e.target.value)}
+						placeholder="Character name"
+					/>
+				</div>
+				<div>
+					<Title>Enter Character Description</Title>
+					<textarea
+						name="Character description"
+						placeholder="Please enter a description of your character"
+						value={description}
+						cols={50}
+						rows={5}
+						onChange={(e) => setDescription(e.target.value)}
+					/>
+				</div>
+				<div>
+					<Title>Pick a class </Title>
+					<div>
+						<Input>
+							<input
+								type="radio"
+								value="KNIGHT"
+								name="class"
+								onChange={() => setCurrentClass("KNIGHT")}
+							/>
+							Knight
+						</Input>
+						<Input>
+							<input
+								type="radio"
+								value="ROGUE"
+								name="class"
+								onChange={() => setCurrentClass("ROGUE")}
+							/>
+							Rogue
+						</Input>
+						<Input>
+							<input
+								type="radio"
+								value="BARBARIAN"
+								name="class"
+								onChange={(e) => setCurrentClass("BARBARIAN")}
+							/>
+							Barbarian
+						</Input>
+						<Input>
+							<input
+								type="radio"
+								value="MAGE"
+								name="class"
+								onChange={(e) => setCurrentClass("MAGE")}
+							/>
+							Mage
+						</Input>
+					</div>
+				</div>
+			</LeftBox>
+			{currentClass === "NONE" ? null : (
+				<RightBox>
+					<CharacterInfo currentClass={currentClass} />
+					<Link to="/home">
+						<button
+							onClick={async () => {
+								const result = await axios.post(
+									"/api/character",
+									{
+										userKey: userId,
+										charName,
+										description,
+										strength: classList[currentClass].str,
+										constitution:
+											classList[currentClass].con,
+										intelligence:
+											classList[currentClass].int,
+										dexterity: classList[currentClass].dex,
+									}
+								)
 
-                                dispatch(
-                                    setCharInfo({
-                                        charName: charName,
-                                        strength: classList[currentClass].str,
-                                        constitution:
-                                            classList[currentClass].con,
-                                        intelligence:
-                                            classList[currentClass].int,
-                                        dexterity: classList[currentClass].dex,
-                                    })
-                                )
+								dispatch(
+									setCharInfo({
+										charName: charName,
+										strength: classList[currentClass].str,
+										constitution:
+											classList[currentClass].con,
+										intelligence:
+											classList[currentClass].int,
+										dexterity: classList[currentClass].dex,
+									})
+								)
 
-                                dispatch(
-                                    setCharId(
-                                        result.data.characterInfo[0]
-                                            .character_key
-                                    )
-                                )
-                            }}
-                        >
-                            Select
-                        </button>
-                    </Link>
-                </RightBox>
-            )}
-        </Background>
-    )
+								dispatch(
+									setCharId(
+										result.data.characterInfo[0]
+											.character_key
+									)
+								)
+							}}
+						>
+							Select
+						</button>
+					</Link>
+				</RightBox>
+			)}
+		</Background>
+	)
 }
-
--- frontend/src/components/Home/User.tsx
+++ frontend/src/components/Home/User.tsx
@@ -4,130 +4,133 @@ import axios from "axios"
 import { useDispatch } from "react-redux"
 import { setCharId, setUserId } from "../../redux/userSlice"
 
-
 const UserContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    justify-content: flex-end;
-    padding: 1em;
-    box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-end;
+	justify-content: flex-end;
+	padding: 1em;
+	box-sizing: border-box;
 `
 
 const UserAvatar = styled.img`
-    image-rendering: pixelated;
-    cursor: pointer;
-    cursor: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAzElEQVRYR+2X0Q6AIAhF5f8/2jYXZkwEjNSVvVUjDpcrGgT7FUkI2D9xRfQETwNIiWO85wfINfQUEyxBG2ArsLwC0jioGt5zFcwF4OYDPi/mBYKm4t0U8ATgRm3ThFoAqkhNgWkA0jJLvaOVSs7j3qMnSgXWBMiWPXe94QqMBMBc1VZIvaTu5u5pQewq0EqNZvIEMCmxAawK0DNkay9QmfFNAJUXfgGgUkLaE7j/h8fnASkxHTz0DGIBMCnBeeM7AArpUd3mz2x3C7wADglA8BcWMZhZAAAAAElFTkSuQmCC)
-            14 0,
-        pointer;
-    border: 4px solid black;
+	image-rendering: pixelated;
+	cursor: pointer;
+	cursor: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAzElEQVRYR+2X0Q6AIAhF5f8/2jYXZkwEjNSVvVUjDpcrGgT7FUkI2D9xRfQETwNIiWO85wfINfQUEyxBG2ArsLwC0jioGt5zFcwF4OYDPi/mBYKm4t0U8ATgRm3ThFoAqkhNgWkA0jJLvaOVSs7j3qMnSgXWBMiWPXe94QqMBMBc1VZIvaTu5u5pQewq0EqNZvIEMCmxAawK0DNkay9QmfFNAJUXfgGgUkLaE7j/h8fnASkxHTz0DGIBMCnBeeM7AArpUd3mz2x3C7wADglA8BcWMZhZAAAAAElFTkSuQmCC)
+			14 0,
+		pointer;
+	border: 4px solid black;
 `
 
 const DropDownMenu = styled.div`
-    position: absolute;
-    top: 10%;
-    right: 1%;
-    width: 12rem;
-    height: auto;
-    background-color: rgba(67, 67, 67, 0.8);
-    z-index: 1000;
-    border: 4px double white;
-    padding: 0.5rem;
+	position: absolute;
+	top: 10%;
+	right: 1%;
+	width: 12rem;
+	height: auto;
+	background-color: rgba(67, 67, 67, 0.8);
+	z-index: 1000;
+	border: 4px double white;
+	padding: 0.5rem;
 `
 
 const DropDownList = styled.ul`
-    list-style: none;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-    justify-content: flex-start;
-    padding: 0;
-    margin: 0;
+	list-style: none;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	text-align: center;
+	justify-content: flex-start;
+	padding: 0;
+	margin: 0;
 `
 
 const DropDownItem = styled.li`
-    color: white;
-    cursor: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAzElEQVRYR+2X0Q6AIAhF5f8/2jYXZkwEjNSVvVUjDpcrGgT7FUkI2D9xRfQETwNIiWO85wfINfQUEyxBG2ArsLwC0jioGt5zFcwF4OYDPi/mBYKm4t0U8ATgRm3ThFoAqkhNgWkA0jJLvaOVSs7j3qMnSgXWBMiWPXe94QqMBMBc1VZIvaTu5u5pQewq0EqNZvIEMCmxAawK0DNkay9QmfFNAJUXfgGgUkLaE7j/h8fnASkxHTz0DGIBMCnBeeM7AArpUd3mz2x3C7wADglA8BcWMZhZAAAAAElFTkSuQmCC)
-            14 0,
-        pointer;
+	color: white;
+	cursor: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAzElEQVRYR+2X0Q6AIAhF5f8/2jYXZkwEjNSVvVUjDpcrGgT7FUkI2D9xRfQETwNIiWO85wfINfQUEyxBG2ArsLwC0jioGt5zFcwF4OYDPi/mBYKm4t0U8ATgRm3ThFoAqkhNgWkA0jJLvaOVSs7j3qMnSgXWBMiWPXe94QqMBMBc1VZIvaTu5u5pQewq0EqNZvIEMCmxAawK0DNkay9QmfFNAJUXfgGgUkLaE7j/h8fnASkxHTz0DGIBMCnBeeM7AArpUd3mz2x3C7wADglA8BcWMZhZAAAAAElFTkSuQmCC)
+			14 0,
+		pointer;
 
-    &:hover {
-        color: #228ed6;
-    }
+	&:hover {
+		color: #228ed6;
+	}
 `
 
 interface Props {}
 
 export default function User(props: Props): ReactElement {
-    const [dropDownEnabled, setDropDownEnabled] = useState(false)
+	const [dropDownEnabled, setDropDownEnabled] = useState(false)
 
-    const dispatch = useDispatch()
+	const dispatch = useDispatch()
 
-    const handleClickAvatar = () => {
-        setDropDownEnabled(!dropDownEnabled)
-    }
+	const handleClickAvatar = () => {
+		setDropDownEnabled(!dropDownEnabled)
+	}
 
-    const handleClickDropDownItem = (item: string) => {
-        switch (item) {
-            case "account":
-                break
-            case "settings":
-                break
-            case "info":
-                const newWindow = window.open(
-                    "https://github.com/the-gamemasters/the-gamemasters/",
-                    "_blank",
-                    "noopener,noreferrer"
-                )
-                if (newWindow) newWindow.opener = null
-                break
-            case "logout":
-                axios.post("/api/logout")
-                    .then(() => {
-                        console.log('logout')
-                        dispatch(setUserId(0))
-                        dispatch(setCharId(0))
-                    })
-                    .catch((err) => {
-                        console.log(err)
-                    })
-                
-                break
-        }
-    }
+	const handleClickDropDownItem = (item: string) => {
+		switch (item) {
+			case "account":
+				break
+			case "settings":
+				break
+			case "info":
+				const newWindow = window.open(
+					"https://github.com/the-gamemasters/the-gamemasters/",
+					"_blank",
+					"noopener,noreferrer"
+				)
+				if (newWindow) newWindow.opener = null
+				break
+			case "logout":
+				axios
+					.post("/api/logout")
+					.then(() => {
+						console.log("logout")
+						dispatch(setUserId(0))
+						dispatch(setCharId(0))
+					})
+					.catch((err) => {
+						console.log(err)
+					})
 
-    return (
-        <UserContainer>
-            <UserAvatar
-                className="nes-avatar is-large "
-                onClick={() => handleClickAvatar()}
-                alt="User avatar"
-                src="https://www.usdairy.com/optimize/getmedia/6ab03180-cc90-4a03-a339-13b540ecc8a5/american.jpg.jpg.aspx"
-            />
-            {!dropDownEnabled ? null : (
-                <DropDownMenu>
-                    <DropDownList>
-                        <DropDownItem
-                            onClick={() => handleClickDropDownItem("account")}>
-                            Account
-                        </DropDownItem>
-                        <DropDownItem
-                            onClick={() => handleClickDropDownItem("settings")}>
-                            Settings
-                        </DropDownItem>
-                        <DropDownItem
-                            onClick={() => handleClickDropDownItem("info")}>
-                            Info
-                        </DropDownItem>
-                        <DropDownItem
-                            onClick={() => handleClickDropDownItem("logout")}>
-                            Logout
-                        </DropDownItem>
-                    </DropDownList>
-                </DropDownMenu>
-            )}
-        </UserContainer>
-    )
+				break
+		}
+	}
+
+	return (
+		<UserContainer>
+			<UserAvatar
+				className="nes-avatar is-large "
+				onClick={() => handleClickAvatar()}
+				alt="User avatar"
+				src="https://www.usdairy.com/optimize/getmedia/6ab03180-cc90-4a03-a339-13b540ecc8a5/american.jpg.jpg.aspx"
+			/>
+			{!dropDownEnabled ? null : (
+				<DropDownMenu>
+					<DropDownList>
+						<DropDownItem
+							onClick={() => handleClickDropDownItem("account")}
+						>
+							Account
+						</DropDownItem>
+						<DropDownItem
+							onClick={() => handleClickDropDownItem("settings")}
+						>
+							Settings
+						</DropDownItem>
+						<DropDownItem
+							onClick={() => handleClickDropDownItem("info")}
+						>
+							Info
+						</DropDownItem>
+						<DropDownItem
+							onClick={() => handleClickDropDownItem("logout")}
+						>
+							Logout
+						</DropDownItem>
+					</DropDownList>
+				</DropDownMenu>
+			)}
+		</UserContainer>
+	)
 }
-
