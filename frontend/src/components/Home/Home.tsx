ac7ed0e94636b51e538fe734846716da803bd5c5
mflanders06 <mflanders06@gmail.com>
2021-11-01 21:44:07 -0600

    prep for merge to master (finished)

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,13 +1,14 @@
 import React, { ReactElement, useEffect, useState } from "react"
+import { Redirect } from "react-router-dom"
 import styled from "styled-components"
 import { useAppDispatch, useAppSelector } from "../../redux/reduxHooks"
 import {
-	selectCharId,
-	selectUserId,
-	selectCharInfo,
-	setCharId,
-	setUserId,
-	setCharInfo,
+    selectCharId,
+    selectUserId,
+    selectCharInfo,
+    setCharId,
+    setUserId,
+    setCharInfo,
 } from "../../redux/userSlice"
 import Character from "./Character"
 import Community from "./Community"
@@ -19,205 +20,210 @@ import axios from "axios"
 interface Props {}
 
 const PageContainer = styled.div`
-	height: 100vh;
-	width: 100vw;
+    height: 100vh;
+    width: 100vw;
 `
 
 const HomeContainer = styled.div`
-	height: 100%;
-	width: 100%;
-	display: grid;
-	grid-template-columns: 1fr;
-	grid-template-rows: 60% 40%;
+    height: 100%;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 60% 40%;
 `
 
 const HomeTop = styled.div`
-	height: 100%;
-	width: 100%;
-	display: grid;
-	grid-template-columns: 1fr 3fr;
-	grid-template-rows: 1fr;
+    height: 100%;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: 1fr;
 `
 
 const HomeBottom = styled.div`
-	height: 100%;
-	width: 100%;
-	display: grid;
-	grid-template-columns: 1fr 2fr;
-	grid-template-rows: 1fr;
+    height: 100%;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    grid-template-rows: 1fr;
 `
 
 const TopRight = styled.div`
-	height: 100%;
-	width: 100%;
-	display: grid;
-	grid-template-columns: 1fr 1fr;
-	grid-template-rows: 20% 80%;
-	background-image: none;
-	background-position: center;
-	background-size: cover;
-	color: white;
+    height: 100%;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 20% 80%;
+    background-image: none;
+    background-position: center;
+    background-size: cover;
+    color: white;
+    border-left: 2px solid black;
+    border-bottom: 2px solid black;
 `
 
 export default function Home(props: Props): ReactElement {
-	const [currentWorld, setCurrentWorld] = useState(2)
-	const charId = useAppSelector(selectCharId)
+    const [currentWorld, setCurrentWorld] = useState(2)
+    const charId = useAppSelector(selectCharId)
 
-	const dispatch = useAppDispatch()
+    const dispatch = useAppDispatch()
 
-	useEffect(() => {
-		const keepCharacterInfoUpdatedOnRedux = async (charId: number) => {
-			try {
-				const result = await axios.get(`/api/character/${charId}`)
+    useEffect(() => {
+        const keepCharacterInfoUpdatedOnRedux = async (charId: number) => {
+            try {
+                const result = await axios.get(`/api/character/${charId}`)
 
-				const {
-					char_name: charName,
-					gold,
-					experience,
-					level,
-					strength,
-					constitution,
-					intelligence,
-					dexterity,
-				} = result.data
-				dispatch(
-					setCharInfo({
-						charName,
-						gold,
-						experience,
-						level,
-						strength,
-						constitution,
-						intelligence,
-						dexterity,
-					})
-				)
-			} catch (error) {
-				console.log(error)
-			}
-		}
-		keepCharacterInfoUpdatedOnRedux(charId)
-	})
+                const {
+                    char_name: charName,
+                    gold,
+                    experience,
+                    level,
+                    strength,
+                    constitution,
+                    intelligence,
+                    dexterity,
+                } = result.data
+                dispatch(
+                    setCharInfo({
+                        charName,
+                        gold,
+                        experience,
+                        level,
+                        strength,
+                        constitution,
+                        intelligence,
+                        dexterity,
+                    })
+                )
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        keepCharacterInfoUpdatedOnRedux(charId)
+    })
 
-	const handleWorldChange = (world: number) => {
-		setCurrentWorld(world)
-	}
+    const handleWorldChange = (world: number) => {
+        setCurrentWorld(world)
+    }
 
-	const getWorldName = () => {
-		return worldList[currentWorld - 1].world_name
-	}
+    const getWorldName = () => {
+        return worldList[currentWorld - 1].world_name
+    }
 
-	return (
-		<PageContainer>
-			<HomeContainer>
-				<HomeTop>
-					<Community />
-					<TopRight
-						style={{
-							backgroundImage: `url(/images/${
-								worldList[currentWorld - 1].background
-							})`,
-						}}
-					>
-						<div>
-							<h2>{getWorldName()}</h2>
-							<h5>{`World ${currentWorld}`}</h5>
-						</div>
-						<User />
-						<div>
-							{/* For Debugging */}
-							<label htmlFor="world_select">Choose World</label>
-							<div className="nes-select is-warning">
-								<select
-									required
-									id="world_select"
-									value={currentWorld}
-									onChange={(e) =>
-										handleWorldChange(
-											Number(e.target.value)
-										)
-									}
-								>
-									<option
-										value=""
-										disabled
-										defaultValue="2"
-										hidden
-									>
-										Select World...
-									</option>
-									<option value="1">
-										World 1: Big Head, Small Arms
-									</option>
-									<option value="2">
-										World 2: Queen of the Nile
-									</option>
-									<option value="3">
-										World 3: Feudal Feud
-									</option>
-									<option value="4">
-										World 4: The Hot War
-									</option>
-									<option value="5">World 5: Overshot</option>
-								</select>
-							</div>
-						</div>
-						<ShopPreview />
-					</TopRight>
-				</HomeTop>
-				<HomeBottom>
-					<Character />
-					<HomeAction />
-				</HomeBottom>
-			</HomeContainer>
-		</PageContainer>
-	)
+    const userId = useAppSelector(selectUserId)
+
+    if (!userId){return (<Redirect to="/" />)}
+    if (!charId){return (<Redirect to="/char" />)}
+
+    return (
+        <PageContainer>
+            <HomeContainer>
+                <HomeTop>
+                    <Community />
+                    <TopRight
+                        style={{
+                            backgroundImage: `url(/images/${
+                                worldList[currentWorld - 1].background
+                            })`,
+                        }}>
+                        <div>
+                            <h2>{getWorldName()}</h2>
+                            <h5>{`World ${currentWorld}`}</h5>
+                        </div>
+                        <User />
+                        <div>
+                            {/* For Debugging */}
+                            <label htmlFor="world_select">Choose World</label>
+                            <div className="nes-select is-warning">
+                                <select
+                                    required
+                                    id="world_select"
+                                    value={currentWorld}
+                                    onChange={(e) =>
+                                        handleWorldChange(
+                                            Number(e.target.value)
+                                        )
+                                    }>
+                                    <option
+                                        value=""
+                                        disabled
+                                        defaultValue="2"
+                                        hidden>
+                                        Select World...
+                                    </option>
+                                    <option value="1">
+                                        World 1: Big Head, Small Arms
+                                    </option>
+                                    <option value="2">
+                                        World 2: Queen of the Nile
+                                    </option>
+                                    <option value="3">
+                                        World 3: Feudal Feud
+                                    </option>
+                                    <option value="4">
+                                        World 4: The Hot War
+                                    </option>
+                                    <option value="5">World 5: Overshot</option>
+                                </select>
+                            </div>
+                        </div>
+                        <ShopPreview currentWorld={currentWorld} />
+                    </TopRight>
+                </HomeTop>
+                <HomeBottom>
+                    <Character />
+                    <HomeAction />
+                </HomeBottom>
+            </HomeContainer>
+        </PageContainer>
+    )
 }
 
 const worldList = [
-	{
-		world_number: 1,
-		world_name: "Big Head, Small Arms",
-		theme: "Triassic Period",
-		boss: "Rex the T",
-		drops_available: "Items",
-		background: "world-1.png",
-		notes: "",
-	},
-	{
-		world_number: 2,
-		world_name: "Queen of the Nile",
-		theme: "Egypt",
-		boss: "Cleopatra",
-		drops_available: "Items, Weapon",
-		background: "world-2.jpg",
-		notes: "",
-	},
-	{
-		world_number: 3,
-		world_name: "Feudal Feud",
-		theme: "Middle Ages",
-		boss: "Lord Maxximus IV",
-		drops_available: "Items, Weapon, Armor",
-		background: "world-3.jpg",
-		notes: "",
-	},
-	{
-		world_number: 4,
-		world_name: "The Hot War",
-		theme: "80's",
-		boss: "Dictator Ronald Reagan",
-		drops_available: "Items, Weapon, Armor",
-		background: "world-4.png",
-		notes: "",
-	},
-	{
-		world_number: 5,
-		world_name: "Overshot",
-		theme: "Future",
-		boss: "(PVP)",
-		drops_available: "Glory",
-		background: "world-5.jpg",
-		notes: "",
-	},
+    {
+        world_number: 1,
+        world_name: "Big Head, Small Arms",
+        theme: "Triassic Period",
+        boss: "Rex the T",
+        drops_available: "Items",
+        background: "world-1.png",
+        notes: "",
+    },
+    {
+        world_number: 2,
+        world_name: "Queen of the Nile",
+        theme: "Egypt",
+        boss: "Cleopatra",
+        drops_available: "Items, Weapon",
+        background: "world-2.jpg",
+        notes: "",
+    },
+    {
+        world_number: 3,
+        world_name: "Feudal Feud",
+        theme: "Middle Ages",
+        boss: "Lord Maxximus IV",
+        drops_available: "Items, Weapon, Armor",
+        background: "world-3.jpg",
+        notes: "",
+    },
+    {
+        world_number: 4,
+        world_name: "The Hot War",
+        theme: "80's",
+        boss: "Dictator Ronald Reagan",
+        drops_available: "Items, Weapon, Armor",
+        background: "world-4.png",
+        notes: "",
+    },
+    {
+        world_number: 5,
+        world_name: "Overshot",
+        theme: "Future",
+        boss: "(PVP)",
+        drops_available: "Glory",
+        background: "world-5.jpg",
+        notes: "",
+    },
 ]
+
--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -2,149 +2,153 @@ import { ReactElement, useState } from "react"
 import styled from "styled-components"
 import AccountCreation from "./AccountCreation"
 import BackgroundMusic from "../General/BackgroundMusic"
-import { Link, useHistory } from "react-router-dom"
+import { Redirect, useHistory } from "react-router-dom"
 import axios from "axios"
-import { useDispatch, useSelector } from "react-redux"
-import { setCharId, setUserId } from "../../redux/userSlice"
+import { useDispatch } from "react-redux"
+import { selectUserId, selectCharId, setCharId, setUserId } from "../../redux/userSlice"
+import { useAppSelector } from "../../redux/reduxHooks"
 
 const LoginContainer = styled.div`
-	padding: 4rem;
-	height: 100vh;
-	width: 100vw;
-	background-image: url("/images/login-background.gif");
-	background-size: cover;
-	background-position: center bottom;
+    padding: 4rem;
+    height: 100vh;
+    width: 100vw;
+    background-image: url("/images/login-background.gif");
+    background-size: cover;
+    background-position: center bottom;
 `
 
 const GameTitle = styled.h1`
-	color: #ffffff;
-	-webkit-text-stroke: 1px black;
+    color: #ffffff;
+    -webkit-text-stroke: 1px black;
 `
 
 const LoginForm = styled.form`
-	padding: 2em 0;
-	width: 40%;
-	height: 50%;
-	display: flex;
-	flex-direction: column;
-	row-gap: 25px;
-	margin: auto;
-	align-items: center;
-	justify-content: space-evenly;
-	margin-bottom: 10vh;
-	background-color: #ffffff;
+    padding: 2em 0;
+    width: 40%;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+    row-gap: 25px;
+    margin: auto;
+    align-items: center;
+    justify-content: space-evenly;
+    margin-bottom: 10vh;
+    background-color: #ffffff;
 
-	input {
-		width: 100%;
-	}
+    input {
+        width: 100%;
+    }
 
-	label,
-	p {
-		margin: 0;
-	}
+    label,
+    p {
+        margin: 0;
+    }
 
-	button {
-		margin-top: 1rem;
-	}
+    button {
+        margin-top: 1rem;
+    }
 `
 
 const ModalContainer = styled.div`
-	position: fixed !important;
-	background-color: white;
-	width: 50%;
-	height: 75%;
-	left: 50%;
-	top: 50%;
-	transform: translate(-50%, -50%);
+    position: fixed !important;
+    background-color: white;
+    width: 50%;
+    height: 75%;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
 `
-
 interface Props {}
 
 export default function Login(props: Props): ReactElement {
-	const [newUser, setNewUser] = useState<boolean>(false)
-	const [email, setEmail] = useState<string>("")
-	const [password, setPassword] = useState<string>("")
-	const history = useHistory()
-	const routeChangeChar = () => {
-		let path = `/Char`
-		history.push(path)
-	}
-	const dispatch = useDispatch()
+    const [newUser, setNewUser] = useState<boolean>(false)
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const dispatch = useDispatch()
+    const history = useHistory();
 
-	async function submitLogin() {
-		if (email.length === 0) {
-			alert("Please enter email and password, or create a new user")
-		} else {
-			await axios
-				.post("/api/login", { email, password })
-				.then((res) => {
-					if (typeof res.data === "string") {
-						console.log(res.data)
-						alert(res.data)
-					} else {
-						console.log(res.data)
-						dispatch(setUserId(res.data.userKey))
-						dispatch(setCharId(res.data.characterKey))
-						routeChangeChar()
-					}
-				})
-				.catch((e) => {
-					console.log(e)
-				})
-		}
-	}
+    function submitLogin() {
+        if (email.length === 0) {
+            alert("Please enter email and password, or create a new user")
+        } else {
+            axios
+                .put("/api/login", { email, password })
+                .then((res) => {
+                    if (typeof res.data === "string") {
+                        console.log(res.data)
+                        alert(res.data)
+                    } else {
+                        console.log(res.data)
+                        dispatch(setUserId(res.data.userKey))
+                        dispatch(setCharId(res.data.characterKey))
+                        history.push("/char");
+                    }
+                })
+                .catch((e) => {
+                    console.log(e)
+                })
+        }
+    }
+    const userId = useAppSelector(selectUserId)
+    const charId = useAppSelector(selectCharId)
 
-	return (
-		<LoginContainer>
-			<BackgroundMusic musicSrc={"audio/music/track11-login.mp3"} />
-			<GameTitle>Time-Battle Dungeons</GameTitle>
-			<LoginForm className="nes-container">
-				<div className="nes-field">
-					<label htmlFor="email">
-						<p className="nes-text is-primary">Email</p>
-					</label>
-					<input
-						type="text"
-						placeholder="Enter Email"
-						id="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						className="nes-input"
-					/>
-				</div>
-				<div className="nes-field">
-					<label htmlFor="password">
-						<p className="nes-text is-primary">Password</p>
-					</label>
-					<input
-						type="text"
-						placeholder="Enter Password"
-						id="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						className="nes-input"
-					/>
-				</div>
+    if (userId){
+        if(charId){return (<Redirect to="/home" />)}
+        return (<Redirect to="/char" />)
+    }
 
-				<button
-					className="nes-btn is-primary"
-					onClick={() => submitLogin()}
-				>
-					Login
-				</button>
-			</LoginForm>
-			<button
-				className="nes-btn is-success"
-				onClick={() => setNewUser(!newUser)}
-			>
-				Create an Account
-			</button>
+    return (
+        <LoginContainer>
+            <BackgroundMusic musicSrc={"audio/music/track11-login.mp3"} />
+            <GameTitle>Time-Battle Dungeons</GameTitle>
+            <LoginForm className="nes-container">
+                <div className="nes-field">
+                    <label htmlFor="email">
+                        <p className="nes-text is-primary">Email</p>
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Enter Email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="nes-input"
+                    />
+                </div>
+                <div className="nes-field">
+                    <label htmlFor="password">
+                        <p className="nes-text is-primary">Password</p>
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Enter Password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="nes-input"
+                    />
+                </div>
 
-			{newUser ? (
-				<ModalContainer className="nes-container">
-					<AccountCreation closeModal={() => setNewUser(false)} />
-				</ModalContainer>
-			) : null}
-		</LoginContainer>
-	)
+                <button
+                    className="nes-btn is-primary"
+                    onClick={() => submitLogin()}
+                >
+                    Login
+                </button>
+            </LoginForm>
+            <button
+                className="nes-btn is-success"
+                onClick={() => setNewUser(!newUser)}
+            >
+                Create an Account
+            </button>
+
+            {newUser ? (
+                <ModalContainer className="nes-container">
+                    <AccountCreation closeModal={() => setNewUser(false)} />
+                </ModalContainer>
+            ) : null}
+        </LoginContainer>
+    )
 }
+
